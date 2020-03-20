6869c45069658bc0dbd3bae243e6395e5a5177c3
Sami-Shiraz1 <52105121+Sami-Shiraz1@users.noreply.github.com>
2020-03-20 07:21:03 -0700

    Other functionality in SQL

---
+++ Nested Query.sql
@@ -0,0 +1,13 @@
+-- On Delete
+
+SELECT * FROM employee
+
+DELETE FROM employee 
+WHERE emp_id = 102
+
+SELECT * FROM branch
+
+SELECT * FROM branch_supplier
+
+DELETE FROM  branch_supplier
+WHERE branch_id = 2
